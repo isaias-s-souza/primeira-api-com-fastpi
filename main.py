8fab0d5473f27511651d7b99f9481f0410ad30a4
Isaias Souza <isaias.souza.spfc@gmail.com>
2022-03-03 01:01:08 -0300

    Validações do método POST

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import email
 from fastapi import FastAPI
 # pydantic é utilizado como validador de dados, já que a classe criada o herda 
 from pydantic import BaseModel
@@ -33,11 +34,26 @@ async def get_usuario_usando_id(id_usuario: int):
         if(usuario.id == id_usuario):
             return usuario
     
-    return {"Status": 404, "Mensagem": "Não encontrou o usuario"}
+    return {"Status": 404, "Mensagem": "Não encontrou o usuario."}
 
 # Rota Insere
 @app.post("/usuarios")
 async def insere_usuario(usuario: Usuario):
-    #criar regras de negocio
+
+    #Validacao campos unicos (Id, Email)
+    for usuarioCadastrado in base_de_dados:
+        if usuarioCadastrado.id == usuario.id:
+            return {"Status": 422, "Mensagem": "ID de usuário já cadastrado."}
+    
+        if usuarioCadastrado.email == usuario.email:
+            return {"Status": 422, "Mensagem": "Email de usuário já cadastrado."}
+    
+    #Validacao Email
+    if usuario.email.find("@") == -1:
+        return {"Status": 422, "Mensagem": "Email inválido, simbolo (@) não encontrado."}       
+
+    if len(usuario.senha) < 5:
+        return {"Status": 422, "Mensagem": "Senha muito curta, a senha deve ter no mínimo 5 caracteres."}  
+
     base_de_dados.append(usuario)
     return usuario
